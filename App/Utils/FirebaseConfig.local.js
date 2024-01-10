c90a042d9f9a7c6ebecf79e965747da873b39f0a
AlpayC <alpaycelik91@googlemail.com>
2024-01-10 12:50:49 +0100

    final project

---
+++ App/Utils/FirebaseConfig.local.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import GlobalApi from "./GlobalApi";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: GlobalApi.FIREBASE_API,
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
